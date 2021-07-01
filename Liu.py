251a621bf851f0156e9575fb502dc644eef1c1eb
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-07-01 06:18:43 +0300

    Update and rename Egnarts3.R to Liu.py

---
+++ Liu.py
@@ -0,0 +1,3 @@
+
+for _ in range(1000000):
+    print("No, emptiness is not nothingness. Emptiness is a type of existence. You must use this existential emptiness to fill yourself.But existance is pain so you must use this type of pain to fill yourself.")
