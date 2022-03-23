63c44434428bcb4baa86010af4f32a352bd7b7ec
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-03-23 06:53:32 +0200

    Update and rename Liu.py to Liu.pas

--- Liu.pas
+++ Liu.pas
@@ -1,3 +1,3 @@
 
-for _ in range(1000000):
+for _ in range(2):
     print("No, emptiness is not nothingness. Emptiness is a type of existence. You must use this existential emptiness to fill yourself.But existance is pain so you must use this type of pain to fill yourself.")
