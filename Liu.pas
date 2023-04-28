4662628d319786b2167651f041cfc6f2989b7a07
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2023-04-28 05:22:59 +0300

    Update and rename Liu.pas to Pythagoras.nb

--- Liu.pas
+++
@@ -1,3 +0,0 @@
-
-for _ in range(2):
-    print("No, emptiness is not nothingness. Emptiness is a type of existence. You must use this existential emptiness to fill yourself.But existance is pain so you must use this type of pain to fill yourself.")
